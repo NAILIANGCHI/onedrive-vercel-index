60e89481b5c31dd8d6de8fdba1e84304e65d19cd
NAILIANGCHI <85297838+NAILIANGCHI@users.noreply.github.com>
2022-11-02 22:17:04 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -45,7 +45,7 @@ module.exports = {
   protectedRoutes: ['/源码备份/Bugle', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'https://2905821826@qq.com',
+  email: '2905821826@qq.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
