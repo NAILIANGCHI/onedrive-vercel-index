f9b2eafaea8dc1a3f1a2a9da2a4f104dd0d7fba7
NAILIANGCHI <85297838+NAILIANGCHI@users.noreply.github.com>
2022-11-02 22:40:18 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['🌞 Private folder/u-need-a-password', '/源码备份/Bugle'],
+  protectedRoutes: ['/源码备份', '/Bugle','壁纸库/Restricted'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '2905821826@qq.com',
